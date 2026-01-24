50ac5ebcd2656b9b0448133fff45354b0a7b3a4a
Kayla Tavares de Almeida <100829218+onetiredkayla@users.noreply.github.com>
2026-01-24 14:41:34 -0500

    base enemy done jump enemy WIP

--- scenes/test/map/ai_test.gd
+++ scenes/test/map/ai_test.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var target = $SubViewportContainer/SubViewport/Player
+@onready var target: CharacterBody3D = $SubViewportContainer/SubViewport/Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	get_tree().call_group("enemy", "_set_player_target", target.global_transform.origin)
+	get_tree().call_group("enemy", "_set_player_target", target)
